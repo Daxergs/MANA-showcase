c876bb597722680ba5a80aa9b8b0dfc6118bfd74
MATEUSZ NAKONIECZNY <3m.innovations.contact@gmail.com>
2023-02-02 10:39:36 +0100

    initial commit

--- js/animations.js
+++ js/animations.js
@@ -3,11 +3,14 @@
 function intro()
 {
     let introbox = document.getElementById("container");
-    let pos = 0;
+    let header = document.querySelector(".header");
+    let invi = 0;
+    let pos = 100;
     let inter = setInterval(up,1);
+    let opacity = setInterval(opa,100);
     function up()
     {
-        if(pos == 300)
+        if(pos == -500)
         {
             clearInterval(inter);
         }
@@ -16,6 +19,20 @@ function intro()
             pos--;
             introbox.style.top=pos+'px';
         }
+    }
+    function opa()
+    {
+        if(pos == -500){
+        if(header.style.opacity == 1)
+        {
+            clearInterval(opacity);
+        }
+        else
+        {
+            invi=invi+0.1;
+            header.style.opacity=invi;
+        }
+    }
     }
 }
 
