2ba87f75f2cd12d4d1504371cd3745cb9b3c1805
MATEUSZ NAKONIECZNY <3m.innovations.contact@gmail.com>
2023-02-06 10:28:58 +0100

    initial commit

--- js/animations.js
+++ js/animations.js
@@ -1,35 +1,53 @@
 let elem = document.querySelector(".animate");
 let experience = document.getElementById("experience");
+let header = document.querySelector(".header");
+let footer = document.querySelector(".footer");
+let websites = document.getElementById("websites");
+let services = document.querySelector(".services");
+
+function store()
+{
+    let storeinter = setInterval(showstore,100);
+    let opa = 0;
+    function showstore()
+    {
+        if(header.style.opacity == 1)
+        {
+            clearInterval(storeinter);
+        }
+        else
+        {
+            opa=opa+0.1;
+            header.style.opacity=opa;
+            footer.style.opacity=opa;
+            services.style.opacity=opa;
+        }
+    }
+}
 
 function intro()
 {
     
     let introbox = document.getElementById("container");
-    let header = document.querySelector(".header");
     let top = document.getElementById("top");
-    let websites = document.getElementById("websites");
     let contact = document.getElementById("contact");
     let faq = document.getElementById("faq");
     let projects = document.getElementById("projects");
-    let footer = document.querySelector(".footer");
     let invi = 0;
     let pos = 100;
     let inter = setInterval(up,1);
     let opacity = setInterval(opa,100);
     function up()
     {
-        if(elem.style.left>="380px")
-        {
         if(pos == -500)
         {
             clearInterval(inter);
         }
         else
         {
-            pos=pos-4;
+            pos=pos-3;
             introbox.style.top=pos+'px';
         }
-    }
     }
     function opa()
     {
@@ -56,14 +74,14 @@ function intro()
 
 function myMove() {
     
-    let pos = -900;
+    let pos = 900;
     let id = setInterval(frame, 100/60);
     function frame() {
-        if (pos == 390) {
+        if (pos == 50) {
             clearInterval(id);
         } else {
-            pos=pos+6;
-            elem.style.left = pos + 'px';
+            pos=pos-2.5;
+            elem.style.top = pos + 'px';
         }
     }
 }
